401785dd5b5dad29f2ea0614def913d6049b54fd
hubnigitle <124351746+hubnigitle@users.noreply.github.com>
2023-11-01 17:23:29 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,56 @@
+const barang = [
+    {
+        NamaBarang : 'Komputer',
+        Jenis      : 'Teknologi',
+        Harga      : 150000000,
+    },
+    {
+        NamaBarang : 'Fingerprint',
+        Jenis      : 'Teknologi',
+        Harga      : 1500000,
+    },
+    {
+        NamaBarang : 'Printer',
+        Jenis      : 'Fasilitas',
+        Harga      : 3000000,
+    },
+    {
+        NamaBarang : 'Meja Kantor',
+        Jenis      : 'Fasilitas',
+        Harga      : 4000000,
+    },
+    {
+        NamaBarang : 'Lemari',
+        Jenis      : 'Fasilitas',
+        Harga      : 3000000,
+    },
+    {
+        NamaBarang : 'Mobil',
+        Jenis      : 'Fasilitas',
+        Harga      : 500000000,
+    },
+    {
+        NamaBarang : 'Saham',
+        Jenis      : 'Keuangan',
+        Harga      : 15000000000,
+    },
+    {
+        NamaBarang : 'Proyektor',
+        Jenis      : 'Teknologi',
+        Harga      : 5000000,
+    },
+    {
+        NamaBarang : 'Lenovo TS 150',
+        Jenis      : 'Teknologi',
+        Harga      : 10000000,
+    },
+    {
+        NamaBarang : 'TV',
+        Jenis      : 'Fasilitas',
+        Harga      : 6000000,
+    },
+    
+    
+];
+
+export default barang;
